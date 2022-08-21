43095d91b175b6f59bdbf033f84891b4c125248b
ElleLeonne <87243032+ElleLeonne@users.noreply.github.com>
2022-08-21 17:02:17 -0500

    Update listen.py
    Next step is building SQLite integration to store conversations

--- listen.py
+++ listen.py
@@ -1,23 +1,42 @@
 import hikari
 import lightbulb
+from lightbulb.ext import tasks
+
 
 bot = lightbulb.BotApp(
     token="",
     default_enabled_guilds=(744554485350924408)
 )
+tasks.load(bot)
+
+
+@tasks.task(m=1, pass_app=True, auto_start=True) #Every 1 minute, run the task
+async def create_message(channel):
+    print("Reply loop Task called.") #print Task to terminal
+    await bot.rest.create_message(channel=744554485350924411, content="Meow") #send message to test server
+
 
 @bot.listen(hikari.GuildMessageCreateEvent) #Basic listen server. Prints anything that is sent in any channel. 
 async def print_message(event):
-    #print(str(event.guild_id)+" in channel "+str(event.channel_id))
+    print(str(event.guild_id)+" in channel "+str(event.channel_id))
     print(str(event.author).rsplit('#')[0]+": "+str(event.content))
 
 
 """
+@tasks.task(m=1, ,auto_start=True)
+async def replyLoop():
+
 @bot.command
 @lightbulb.command('ping', 'Says pong!')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def ping(context):
     await context.respond('Pong!')
+
+@tasks.task(m=1, pass_app=True, auto_start=True)
+async def create_message(channel, content)
+    await bot.rest.create_message(channel, content)
+
+await bot.rest.create_message(channel_id, message)
 
 @bot.command
 @lightbulb.command('group', 'This is a group')
@@ -37,7 +56,5 @@ bot = hikari.GatewayBot(token="")
 async def print_message(event):
     return
 """
-x = x + 1
-print(X)
 
 bot.run()
