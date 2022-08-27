8219931ef1c551fb9b4078715fc0d8a577490a8a
ElleLeonne <87243032+ElleLeonne@users.noreply.github.com>
2022-08-27 12:52:16 -0500

    Added whitelist format

--- __init__.py
+++ __init__.py
@@ -1,4 +1,11 @@
 __version__ = 0.0
 
-ELLE_GUILD_ID = 744554485350924408 #Elle's Server
-ELLE_GENERAL_CHANNEL = 744554485350924411
+WHITELIST = [
+[744554485350924408, "Elle''s Server", 0.1, [
+    [744554485350924411, "General", 0],
+    [971142805776900126, "Trading", 0]]],
+[XXXXXXXXXXXXX, "Filler Server", 0.2, [
+    [XXXXXXXXXXXX, "Conversation", 0],
+    [XXXXXXXXXXXX, "Gaming Chat", 0]]]
+]
+#Format: [Guild ID, Description, Response Chance, [Channel Whitelist, Description, Response Check]]
