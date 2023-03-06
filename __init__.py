4b87200883266c7e9db0b92e30d350bd26d338d7
Lily <87243032+ElleLeonne@users.noreply.github.com>
2023-03-05 19:40:53 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,44 @@
-__version__ = 0.0
+__version__ = 0.1
 
-WHITELIST = [
-[744554485350924408, "Elle''s Server", 0.1, [
-    [744554485350924411, "General", 0],
-    [971142805776900126, "Trading", 0]]],
-[XXXXXXXXXXXXX, "Filler Server", 0.2, [
-    [XXXXXXXXXXXX, "Conversation", 0],
-    [XXXXXXXXXXXX, "Gaming Chat", 0]]]
-]
-#Format: [Guild ID, Description, Response Chance, [Channel Whitelist, Description, Response Check]]
+WHITELIST = {
+	"ServerList": {
+		111111111111111111: {
+			"Name": "Anon's Server",
+            "Channels": {
+				111111111111112: 
+                    {"Name":"General",
+                    "Type":"Channel",
+                    "Reply":False,
+                    "ResponseChance": 1,
+                    "Server": 111111111111111111},
+                111111111111113:
+                    {"Name":"Jokes",
+                    "Type":"Channel",
+                    "Reply":False,
+                    "ResponseChance": 0.1,
+                    "Server": 111111111111111111}
+			},
+
+		},
+        211111111111111111: {
+			"Name": 'Duck Server',
+            "Channels": {
+				211111111111111112:
+                {"Name":"Duck Channel",
+                 "Type":"Channel",
+                 "Reply":False,
+                 "ResponseChance": 0.9,
+                 "Server": 211111111111111111}
+			}
+
+		}
+	},
+	"UserList": {21212121212121121:
+                    {"Name":"Me",
+                    "Type":"DM",
+                    "Reply":False,
+                    "ResponseChance": 1,
+                    "Server":0000000000}
+	}
+}
+
